98043e71ac2c911e8e1eb123b9487a669fc9b58d
Gasan Omarov <omarov.g.r.6@gmail.com>
2024-03-04 01:40:34 +0300

    helped

--- Services/DiseaseService.cs
+++ Services/DiseaseService.cs
@@ -15,7 +15,20 @@ public class DiseaseService
 
     public void CreateDisease(DiseaseDTO newDisease)
     {
-        diseaseRepo.Create(ToDisease(newDisease));
+        using (var context = new ApplicationContext())
+        {
+            Patient patient = context.Patients
+                .Where(p => p.FirstName == newDisease.PatientFirstName && p.LastName == newDisease.PatientLastName)
+                .First();
+            Disease disease = new Disease(
+                patient,
+                DateOnly.Parse(newDisease.StartDay),
+                DateOnly.Parse(newDisease.StopDay),
+                newDisease.Desctiprion
+            );
+            context.Diseases.Add(disease);
+            context.SaveChanges();
+        }
     }
 
     public DiseaseDTO? GetDiseaseById(int id)
@@ -46,23 +59,31 @@ public class DiseaseService
 
     private Disease ToDisease(DiseaseDTO diseaseDTO)
     {
-        PatienRepo patienRepo = new();
-        return new Disease(
-            int.Parse(diseaseDTO.Id),
-            patienRepo.GetByFirstnameAndLastname(diseaseDTO.PatientFirstName, diseaseDTO.PatientLastName),
-            DateOnly.Parse(diseaseDTO.StartDay),
-            DateOnly.Parse(diseaseDTO.StopDay),
-            diseaseDTO.Desctiprion
-        );
+        using (ApplicationContext context = new())
+        {
+            Patient patient = context.Patients
+                .Where(p => p.FirstName == diseaseDTO.PatientFirstName && p.LastName == diseaseDTO.PatientLastName)
+                .First();
+
+            return new Disease(
+                diseaseDTO.Id is null ? 0 : int.Parse(diseaseDTO.Id),
+                patient,
+                DateOnly.Parse(diseaseDTO.StartDay),
+                DateOnly.Parse(diseaseDTO.StopDay),
+                diseaseDTO.Desctiprion
+            );
+        }
     }
 
     private DiseaseDTO? ToDiseaseDTO(Disease? disease)
     {
+        PatienRepo patienRepo = new();
+        Patient? patient = patienRepo.GetById(disease.PatientId);
         return disease is null ? null :
             new DiseaseDTO(
                 disease.Id.ToString(),
-                disease.Patient.FirstName,
-                disease.Patient.LastName,
+                patient.FirstName,
+                patient.LastName,
                 disease.Start.ToString(),
                 disease.Stop.ToString(),
                 disease.Desctiprion
